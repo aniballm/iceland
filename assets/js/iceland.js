2a4a6e93f507fc2ffaaad15e53680903ebff3b8d
Aníbal Liñán Martínez <anibal@gold.barcelona>
2023-03-09 14:07:21 +0100

    Correcciones en la maqueta + Animaciones

--- assets/js/iceland.js
+++ assets/js/iceland.js
@@ -1,19 +1,53 @@
+//import MagnetMouse from './magnet-mouse'
+
 /* Animación del botón del menú */
 const menuHamburger = document.querySelector('#navicon')
+const menuScreen = document.querySelector('.menu')
 
 menuHamburger.addEventListener('click', () => {
     if (menuHamburger.classList.contains('open')) {
-        menuHamburger.classList.remove('open')
+        menuScreen.classList.remove('open')
+        menuScreen.classList.add('close')
+
+        setTimeout(() => {
+            menuScreen.classList.remove('close')
+            menuHamburger.classList.remove('open')
+        }, 1000)
     } else {
         menuHamburger.classList.add('open')
+        if (!menuScreen.classList.contains('open')) {
+            menuScreen.classList.add('open')
+        }
     }
 })
 
-
-/* Movimiento del logo */
-const logoContainer = document.querySelector('.header-logo')
-
-logoContainer.addEventListener('mousemove',(e) => {
-    console.log(e.target)
-    console.log('X: ' + e.movementX + ' Y: ' + e.movementY)
+/* Animaciones de los elementos principales */
+const logoAnimation = new MagnetMouse({
+    magnet: {
+        element: '.header-logo',
+        enabled: true,
+        distance: 10,
+        position: 'center'
+    }
 })
+logoAnimation.init()
+
+const menuAnimation = new MagnetMouse({
+    magnet: {
+        element: '.header-menu',
+        enabled: true,
+        distance: 10,
+        position: 'center'
+    }
+})
+menuAnimation.init()
+
+const titleAnimation = new MagnetMouse({
+    magnet: {
+        element: '.title-text',
+        enabled: true,
+        distance: 10,
+        position: 'center'
+    }
+})
+titleAnimation.init()
